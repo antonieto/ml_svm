ec7d939bef11b80866143685ceacc618becc1073
fynnMe <115350159+fynnMe@users.noreply.github.com>
2023-12-15 17:17:36 +0100

    less output

--- DigitRecognizer/plot_row.R
+++ DigitRecognizer/plot_row.R
@@ -3,7 +3,7 @@ library(readr)
 # Specify the row to plot
 row <- 10
 
-train <- read_csv("../train.csv")
+train <- read_csv("../train.csv", show_col_types = FALSE)
 train[1, 1]
 img <- matrix(as.vector(train[row, -1]), nrow = 28, ncol = 28, byrow = TRUE)
 img <- apply(img, 1:2, as.numeric)
