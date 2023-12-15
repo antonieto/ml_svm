bf35010d3c2cabf1345f348913bdb7039af33699
fynnMe <115350159+fynnMe@users.noreply.github.com>
2023-12-15 17:07:02 +0100

    add plot row script

---
+++ DigitRecognizer/plot_row.R
@@ -0,0 +1,15 @@
+library(readr)
+
+# Specify the row to plot
+row <- 10
+
+train <- read_csv("../train.csv")
+train[1, 1]
+img <- matrix(as.vector(train[row, -1]), nrow = 28, ncol = 28, byrow = TRUE)
+img <- apply(img, 1:2, as.numeric)
+
+# Rotate the image 90 degrees clockwise
+rotated_img <- t(apply(img, 2, rev))
+
+# Display the rotated image
+image(rotated_img, main = paste("Row", row, ""))
