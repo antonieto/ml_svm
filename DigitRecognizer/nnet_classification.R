8e7d17d5a58f96c9d00d670edcb432aecab89398
fynnMe <115350159+fynnMe@users.noreply.github.com>
2023-12-16 16:22:52 +0100

    añadir puta script

--- DigitRecognizer/nnet_classification.R
+++ DigitRecognizer/nnet_classification.R
@@ -1,9 +1,6 @@
 library(caret)
-library(rpart)
 library(nnet)
-library(e1071)
-
-set.seed(123)
+library(pROC)
 
 data_titanic <- read.csv("Titanic_Lab3/train.csv", header=T)
 
@@ -17,7 +14,7 @@ data_titanic$Ticket=NULL
 data_titanic$Cabin=NULL
 
 # Validacion cruzada
-set.seed(200)
+set.seed(120)
 d_size<-dim(data_titanic)[1]
 dtest_size <- ceiling(0.2*d_size)
 samples<-sample(d_size, d_size, replace = FALSE)
@@ -30,8 +27,9 @@ dtest<-data_titanic[indexes,]
 
 
 # Encontrar mejor valor para size
-max_size <- 50
+max_size <- 30
 accuracy_vector <- numeric(max_size)
+auc_vector <- numeric(max_size)
 
 for (size in 1:max_size) {
   perceptronTitanic <- nnet(Survived ~ ., data = dtrain, size = size, trace = FALSE)
@@ -49,10 +47,24 @@ for (size in 1:max_size) {
   accuracy_vector[size] <- accuracy
   
   print(paste("Accuracy size =", size, ":", accuracy))
+  
+  # Calculate AUC
+  roc_curve <- roc(dtest$Survived, as.numeric(predictions))
+  auc <- auc(roc_curve)
+  auc_vector[size] <- auc
+  
+  # Store accuracy in the vector
+  auc_vector[size] <- auc
+  
+  print(paste("AUC size =", size, ":", auc))
 }
 
 # Plot accuracy vs size
 plot(1:max_size, accuracy_vector, type = "l", col = "blue", xlab = "Size", ylab = "Accuracy",
      main = "Accuracy vs Size for Neural Network")
 
-# plan: cada uno entrena un clasificador cambiando los parametros -> jupyter notebook
+# Plot auc vs size
+plot(1:max_size, auc_vector, type = "l", col = "blue", xlab = "Size", ylab = "AUC",
+     main = "AUC vs Size for Neural Network")
+
+# -> Elegimos size = 15 (Accuracy = 0.664335664335664)
